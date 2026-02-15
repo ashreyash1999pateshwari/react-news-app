238fbe59710e36a4a30655cdc3da240ba1eb7dcd
Ashreyash <pateshwaria@gmail.com>
2026-02-16 04:38:53 +0530

    fixed ui issues

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,7 +6,7 @@ export class NewsItems extends Component {
         let {title,description,imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",newsUrl} = this.props;
         return (
             <div>
-                <div className="card my-3" style={{width: "18rem"}}>
+                <div className="card my-3 mx-auto">
                     <img src={imageUrl?imageUrl:"https://s.hdnux.com/photos/01/63/16/61/30207079/6/rawImage.jpg"} className="card-img-top" alt="crickImg"/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
